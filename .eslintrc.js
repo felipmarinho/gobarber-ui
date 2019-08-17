022e5281a252e01213ee4d14d173232c5a1103ff
Felipe Marinho <felipmarinho@gmail.com>
2019-08-17 05:36:25 -0300

    Adding authentication for providers

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,6 +33,7 @@ module.exports = {
 
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
+    'react/jsx-props-no-spreading': 'off',
 
     'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
     'react/jsx-one-expression-per-line': 'off',
